c278d4d505426fade0e1c0b46aa2c2e0871b9f53
Kevin Matthews <kvnrmatthews@gmail.com>
2021-07-20 10:07:30 -0700

    Separate out computer player functions and player selection into their own files/components.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import { useState, useEffect } from 'react'
 
 import Header from './Header.js'
+import PlayerSelection from './PlayerSelection.js'
+
+import { computerColour, findNextPlay } from './computerPlayers.js'
 
 import './css/index.css'
 
@@ -23,22 +26,22 @@ function App() {
   const [toPlay, setToPlay] = useState(-1);
   /* 0, 1 for player win, -1 for not yet, 2 for draw */
   const [outcome, setOutcome] = useState(-1);
-  /* for temporary storage when creating a player */
-  const [player, setPlayer] = useState({});
 
 
   /* effects */
 
   useEffect(() => {
+    // if computer's turn, do it's move
     if (toPlay >= 0 && players[toPlay].type === "computer") {
       // TODO: add proper strategy instead of random
-      let {row, col} = findNextPlay(players[toPlay].difficulty);
+      let {row, col} = findNextPlay(players[toPlay].difficulty, board);
       setTimeout(() => {
         placePiece(row, col);
       }, 700 + players[toPlay].difficulty * 350);
     }
   }, [toPlay]);
   useEffect(() => {
+    // reselect a computer colour if necessary to avoid conflict
     if (players.length === 2 && players[0].colour === players[1].colour) {
       if (players[1].type === "computer") {
         let newPlayers = [...players];
@@ -67,35 +70,6 @@ function App() {
     setBoard(newBoard);
     setOutcome(-1);
     setToPlay(-1);
-  }
-  function computerName(diff) {
-    switch (diff) {
-      case 0:
-        return "Rookie Ron";
-      case 1:
-        return "Seasoned Sam";
-      case 2:
-        return "Expert Ellie";
-      default:
-        return "Error Erik";
-    }
-  }
-  function computerColour(diff) {
-    switch (diff) {
-      case 0:
-        return "green";
-      case 1:
-        return "yellow";
-      case 2:
-        return "red";
-      default:
-        return "purple";
-    }
-  }
-  function isColour(str){
-    let s = new Option().style;
-    s.color = str;
-    return s.color !== '';
   }
 
   function placePiece(r_ind, c_ind) {
@@ -136,142 +110,17 @@ function App() {
   }
 
 
-  /* computer player strategies */
-  function findNextPlay(diff) {
-    var row = 1;
-    var col = 1;
-    while (board[row][col] !== -1) {
-      row = Math.floor(Math.random() * 3);
-      col = Math.floor(Math.random() * 3);
-    }
-    return {row, col}
-  }
-
-
   /* return */
   // TODO: split up into components once the structure is better understood
 
   if (players.length < 2) {
     /* create/select a player */
     return (
-      <>
-        {players.length === 0 ?
-          <div>First Player:</div> :
-          <div>Second Player:</div>
-        }
-        <button type="button" onClick={() => {
-            setPlayer({type: "human"})
-          }}
-        >
-        Player-Controlled
-        </button>
-        <button type="button" onClick={() => {
-            setPlayer({type: "computer"})
-          }}
-        >
-        Computer-Controlled
-        </button>
-        {
-          player.type === "computer" && (
-            <form>
-              <label htmlFor="difficulty">
-                Difficulty:
-                <select
-                  id="difficulty"
-                  name="difficulty"
-                  value={player.difficulty}
-                  onChange={(e) => {
-                    setPlayer({...player, difficulty: e.target.value});
-                  }}
-                >
-                  <option value="">Random</option>
-                  <option value="0">Easy</option>
-                  <option value="1">Medium</option>
-                  <option value="2">Hard</option>
-                </select>
-              </label>
-              <input
-                type="submit"
-                value="Submit"
-                onClick={(e) => {
-                  e.preventDefault();
-                  let diff = (
-                    !player.difficulty
-                  ) ? (
-                    Math.floor(Math.random() * 3)
-                  ) : (
-                    parseInt(player.difficulty, 10)
-                  );
-                  let newPlayers = [...players];
-                  newPlayers.push({
-                    name: computerName(diff),
-                    type: "computer",
-                    difficulty: diff,
-                    colour: computerColour(diff),
-                    wins: 0,
-                    draws: 0,
-                    loses: 0
-                  })
-                  setPlayers(newPlayers);
-                  setPlayer({});
-                }}
-              />
-            </form>
-          )
-        }
-
-        {
-          player.type === "human" && (
-            <form>
-              <label htmlFor="name">
-                Name:
-                <input
-                  type="text"
-                  id="name"
-                  name="name"
-                  placeholder="Enter a display name..."
-                  value={player.name}
-                  onChange={(e) => {
-                    setPlayer({...player, name: e.target.value});
-                  }}
-                />
-              </label>
-              <label htmlFor="colour">
-                Colour:
-                <input
-                  type="text"
-                  id="colour"
-                  name="colour"
-                  placeholder="Pick your piece colour..."
-                  value={player.colour}
-                  onChange={(e) => {
-                    setPlayer({...player, colour: e.target.value.toLowerCase()});
-                  }}
-                />
-              </label>
-              <input
-                type="submit"
-                value="Submit"
-                onClick={(e) => {
-                  e.preventDefault();
-                  let newPlayers = [...players];
-                  newPlayers.push({
-                    name: player.name,
-                    colour: player.colour,
-                    type: "human",
-                    wins: 0,
-                    draws: 0,
-                    loses: 0
-                  })
-                  !isColour(player.colour) && alert("Enter a valid colour.");
-                  isColour(player.colour) && setPlayers(newPlayers);
-                  isColour(player.colour) && setPlayer({});
-                }}
-              />
-            </form>
-          )
-        }
-      </>
+      <PlayerSelection
+        players={players}
+        setPlayers={setPlayers}
+        computerColour={computerColour}
+      />
     )
 
 
---
+++ src/computerPlayers.js
@@ -0,0 +1,42 @@
+
+
+// names of computer players
+function computerName(diff) {
+  switch (diff) {
+    case 0:
+      return "Rookie Ron";
+    case 1:
+      return "Seasoned Sam";
+    case 2:
+      return "Expert Ellie";
+    default:
+      return "Error Erik";
+  }
+}
+
+// colours of computer players, and default backup for conflicts
+function computerColour(diff) {
+  switch (diff) {
+    case 0:
+      return "green";
+    case 1:
+      return "yellow";
+    case 2:
+      return "red";
+    default:
+      return "purple";
+  }
+}
+
+/* computer player strategies */
+function findNextPlay(diff, board) {
+  var row = 1;
+  var col = 1;
+  while (board[row][col] !== -1) {
+    row = Math.floor(Math.random() * 3);
+    col = Math.floor(Math.random() * 3);
+  }
+  return {row, col}
+}
+
+export { computerName, computerColour, findNextPlay };
