ef97fd76d1ef7f8ea5412fb386c610c761bc64a2
Kevin Matthews <kvnrmatthews@gmail.com>
2021-07-21 22:35:21 -0700

    Separate single square on board into its own component.

--- src/Board.js
+++ src/Board.js
@@ -1,23 +1,48 @@
+function BoardSquare({
+  square,
+  r_ind,
+  c_ind,
+  players,
+  outcome,
+  toPlay,
+  placePiece,
+}) {
+  if (square !== -1) {
+    /* piece already placed here */
+    return <span> .{players[square].piece}. </span>;
+  } else if (outcome === -1 && players[toPlay].type === "human") {
+    /* no piece, is valid move */
+    return (
+      <button
+        key={c_ind}
+        type="button"
+        onClick={() => placePiece(r_ind, c_ind)}
+      >
+        ..
+      </button>
+    );
+  } else {
+    /* no piece, somebody already won */
+    return <span key={c_ind}> .... </span>;
+  }
+}
+
 function Board({ board, players, toPlay, outcome, placePiece }) {
   return board.map((row, r_ind) => {
     return (
       <div key={r_ind}>
         {row.map((square, c_ind) => {
-          return square !== -1 ? (
-            /* piece already placed here */
-            <span key={c_ind}> .{players[square].piece}. </span>
-          ) : outcome === -1 && players[toPlay].type === "human" ? (
-            /* no piece, is valid move */
-            <button
+          return (
+            <BoardSquare
               key={c_ind}
-              type="button"
-              onClick={() => placePiece(r_ind, c_ind)}
-            >
-              ..
-            </button>
-          ) : (
-            /* no piece, somebody already won */
-            <span key={c_ind}> .... </span>
+              square={square}
+              r_ind={r_ind}
+              c_ind={c_ind}
+              players={players}
+              outcome={outcome}
+              toPlay={toPlay}
+              placePiece={placePiece}
+            />
           );
         })}
       </div>
